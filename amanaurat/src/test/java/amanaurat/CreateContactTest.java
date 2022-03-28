853c32457fec1b7d56a31ca6c69dc91c421a4a7f
Ankita Verma <Ankita Verma@LAPTOP-2B9BFCAM>
2022-03-28 15:07:29 +0530

    abc

---
+++ amanaurat/src/test/java/amanaurat/CreateContactTest.java
@@ -0,0 +1,13 @@
+package amanaurat;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void createcontact()
+	{
+		System.out.println("contact is created");
+	}
+
+}
